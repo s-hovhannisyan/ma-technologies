9f1826d806135b0d8cda185192462cae0023609a
Sargis Hovhannisyan <s.m.hovhannisyan@gmail.com>
2024-07-13 22:21:37 +0400

    some changes

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import logo from './assets/logo.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="wrap">
+        <div className="container">
+          <img src={logo} />
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
